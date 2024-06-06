76d2b5cffeba70375bc587885f0999d9e87daf30
oxiEva <oxieva78@gmail.com>
2024-06-06 17:17:44 +0200

    Add mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,16 @@ module.exports = {
   e2e: {
     chromeWebSecurity: false,
     baseUrl: 'https://www.saucedemo.com/',
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'cypress-university-qa',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
